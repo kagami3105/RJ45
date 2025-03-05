e8bffc7eb585ee824f5a7deb9235bdd7d87f5f0f
Kagami <kagami@example.com>
2025-03-05 14:47:47 -0400

    oriented programming objet

--- src/main.cpp
+++ src/main.cpp
@@ -6,196 +6,132 @@
   Version    : 0.0.1
 */
 
-#include <Arduino.h>
 
-// Définition des broches pour les 8 fils du câble RJ45
-const int pinsTX[] = {2, 3, 4, 5, 6, 7, 8, 9};         // Broches pour l'émetteur
-const int pinsRX[] = {10, 11, 12, 13, A0, A1, A2, A3}; // Broches pour le récepteur
+#include <Arduino.h>
+// #include <ArduinoUnit.h>
+
+class TesteurCable
+{
+protected:
+    const int *broches;
+    int brocheCount;
+
+public:
+    TesteurCable(const int *_broches, int _brocheCount) : broches(_broches), brocheCount(_brocheCount) {}
+    virtual void setup() = 0;
+    virtual void loop() = 0;
+};
+
+class Emetteur : public TesteurCable
+{
+public:
+    Emetteur(const int *_broches, int _brocheCount) : TesteurCable(_broches, _brocheCount) {}
+    void setup() override
+    {
+        for (int i = 0; i < brocheCount; i++)
+        {
+            pinMode(broches[i], OUTPUT);
+        }
+    }
+    void loop() override
+    {
+        for (int i = 0; i < brocheCount; i++)
+        {
+            digitalWrite(broches[i], HIGH);
+            delay(10);
+            digitalWrite(broches[i], LOW);
+        }
+        delay(500);
+    }
+};
+
+class Recepteur : public TesteurCable
+{
+public:
+    Recepteur(const int *_broches, int _brocheCount) : TesteurCable(_broches, _brocheCount) {}
+    void setup() override
+    {
+        for (int i = 0; i < brocheCount; i++)
+        {
+            pinMode(broches[i], INPUT);
+        }
+    }
+
+    String DetecteurCable()
+    {
+        int tab[8];
+        for (int i = 0; i < brocheCount; i++)
+        {
+            tab[i] = digitalRead(broches[i]);
+        }
+        bool type1 = true;
+        bool type2 = true;
+
+        for (int i = 0; i < brocheCount; i++)
+        {
+            if (tab[i] != HIGH)
+                type1 = false;
+            if (tab[brocheCount - 1 - i] != HIGH)
+                type2 = false;
+        }
+        if (type1)
+            return ("Câble droit");
+        if (type2)
+            return ("Câble croisé");
+        return ("Câble non détecté");
+    }
+    void loop() override
+    {
+        Serial.print("Câble détecté : ");
+        Serial.println(DetecteurCable());
+        delay(500);
+    }
+};
+
+const int testBroches[8] = {2, 3, 4, 5, 6, 7, 8, 9};
+Emetteur emetteur(testBroches, 8);
+Recepteur recepteur(testBroches, 8);
 
 void setup()
 {
-  Serial.begin(9600);
-
-  // Configuration des broches TX en sortie et RX en entrée
-  for (int i = 0; i < 8; i++)
-  {
-    pinMode(pinsTX[i], OUTPUT);
-    pinMode(pinsRX[i], INPUT_PULLUP);
-  }
+    Serial.begin(9600);
+    emetteur.setup();
+    recepteur.setup();
+    // Test::run();
 }
 
 void loop()
 {
-  bool isStraight = true;
-  bool isCrossed = true;
-
-  for (int i = 0; i < 8; i++)
-  {
-    // Activer une broche TX à la fois
-    digitalWrite(pinsTX[i], LOW);
-    delay(10);
-
-    // Vérifier les connexions
-    for (int j = 0; j < 8; j++)
-    {
-      if (digitalRead(pinsRX[j]) == LOW)
-      {
-        if (i != j)
-          isStraight = false;
-        if ((i == 0 && j != 2) || (i == 1 && j != 3) || (i == 2 && j != 0) || (i == 3 && j != 1) ||
-            (i >= 4 && i != j))
-          isCrossed = false;
-      }
-    }
-
-    digitalWrite(pinsTX[i], HIGH);
-    delay(10);
-  }
-
-  // Afficher le résultat
-  if (isStraight)
-  {
-    Serial.println("Le câble est droit");
-  }
-  else if (isCrossed)
-  {
-    Serial.println("Le câble est croisé");
-  }
-  else
-  {
-    Serial.println("Câble non reconnu ou défectueux");
-  }
-
-  delay(2000); // Attendre 2 secondes avant le prochain test
+    emetteur.loop();
+    recepteur.loop();
 }
 
-// // Pins pour envoyer le signal (émetteur)
-// const int outputPins[8] = {2, 3, 4, 5, 6, 7, 8, 9};
-
-// // Pins pour recevoir le signal (récepteur)
-// const int inputPins[8] = {A0, A1, A2, A3, A4, A5, 10, 11};
-
-// // Tableau pour stocker les correspondances détectées
-// int connections[8] = {-1, -1, -1, -1, -1, -1, -1, -1}; // -1 signifie non connecté
-
-// void setup()
+// test(detectionCableType_Droit)
 // {
-//   // Configurer les pins de sortie
-//   for (int i = 0; i < 8; i++)
-//   {
-//     pinMode(outputPins[i], OUTPUT);
-//     digitalWrite(outputPins[i], LOW);
-//   }
-
-//   // Configurer les pins d'entrée
-//   for (int i = 0; i < 8; i++)
-//   {
-//     pinMode(inputPins[i], INPUT);
-//   }
-
-//   // Démarrer la communication série
-//   Serial.begin(9600);
-//   Serial.println("Testeur de câble RJ45 démarré");
+//     int array[8] = {HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH};
+//     for (int i = 0; i < 8; i++)
+//     {
+//         digitalWrite(testBroches[i], array[i]);
+//     }
+//     assertEqual(recepteur.DetecteurCable(), "Câble droit");
 // }
 
-// void testCable()
+// test(detectionCableType_Croise)
 // {
-//   // Réinitialiser les correspondances
-//   for (int i = 0; i < 8; i++)
-//   {
-//     connections[i] = -1;
-//   }
-
-//   // Tester chaque fil
-//   for (int i = 0; i < 8; i++)
-//   {
-//     digitalWrite(outputPins[i], HIGH); // Activer une sortie
-//     delay(10);                         // Petit délai pour stabiliser
-
-//     // Vérifier toutes les entrées
-//     for (int j = 0; j < 8; j++)
+//     int array[8] = {HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH};
+//     for (int i = 0; i < 8; i++)
 //     {
-//       if (digitalRead(inputPins[j]) == HIGH)
-//       {
-//         connections[i] = j; // Stocker la correspondance
-//         break;              // Sortir dès qu'une connexion est trouvée
-//       }
+//         digitalWrite(testBroches[7 - i], array[i]);
 //     }
-
-//     digitalWrite(outputPins[i], LOW); // Désactiver la sortie
-//     delay(10);
-//   }
-
-//   // Analyser le résultat
-//   analyzeCable();
+//     assertEqual(recepteur.DetecteurCable(), "Câble croisé");
 // }
 
-// void analyzeCable()
+// test(test_detectionCableType_None)
 // {
-//   Serial.println("\nRésultat du test :");
-
-//   // Vérifier si tous les fils sont connectés
-//   bool allConnected = true;
-//   for (int i = 0; i < 8; i++)
-//   {
-//     if (connections[i] == -1)
+//     int array[8] = {LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW};
+//     for (int i = 0; i < 8; i++)
 //     {
-//       allConnected = false;
-//       Serial.print("Fil ");
-//       Serial.print(i + 1);
-//       Serial.println(" : non connecté (coupé)");
+//         digitalWrite(testBroches[i], array[i]);
 //     }
-//     else
-//     {
-//       Serial.print("Fil ");
-//       Serial.print(i + 1);
-//       Serial.print(" -> Entrée ");
-//       Serial.println(connections[i] + 1);
-//     }
-//   }
-
-//   if (!allConnected)
-//   {
-//     Serial.println("Câble défectueux : au moins un fil est coupé.");
-//     return;
-//   }
-
-//   // Vérifier si le câble est droit (straight)
-//   bool isStraight = true;
-//   for (int i = 0; i < 8; i++)
-//   {
-//     if (connections[i] != i)
-//     {
-//       isStraight = false;
-//       break;
-//     }
-//   }
-
-//   if (isStraight)
-//   {
-//     Serial.println("Câble droit (straight) détecté.");
-//     return;
-//   }
-
-//   // Vérifier si le câble est croisé (crossover T568A/T568B classique)
-//   bool isCrossover = (connections[0] == 2 && connections[1] == 5 &&
-//                       connections[2] == 0 && connections[5] == 1 &&
-//                       connections[3] == 3 && connections[4] == 4 &&
-//                       connections[6] == 6 && connections[7] == 7);
-
-//   if (isCrossover)
-//   {
-//     Serial.println("Câble croisé (crossover T568A/T568B) détecté.");
-//   }
-//   else
-//   {
-//     Serial.println("Câble ni droit ni croisé standard : configuration personnalisée ou erreur.");
-//   }
-// }
-
-// void loop()
-// {
-//   Serial.println("\nNouveau test...");
-//   testCable();
-//   delay(5000); // Attendre 5 secondes avant de relancer le test
+//     assertEqual(recepteur.DetecteurCable(), "Câble non détecté");
 // }
